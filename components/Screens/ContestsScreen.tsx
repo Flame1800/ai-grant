7af60e93902888f6983fe3e51566276b12b928cf
Flame1800 <r.shaficoff@gmail.com>
2024-11-30 20:40:24 +0500

    fetch contest

--- components/Screens/ContestsScreen.tsx
+++ components/Screens/ContestsScreen.tsx
@@ -1,7 +1,10 @@
 import { mockContests } from "@/components/Сontests/mockContests";
 import { ContestCard } from "@/components/Сontests/ContestCard/ContestCard";
+import {useState} from "react";
+import {cookies} from "next/headers";
 
-export function ContestsScreen() {
+export async function ContestsScreen() {
+    const cookieStore = cookies();
     return (
         <div className="gap-6 screen-w">
             <h2 className="mt-10 scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0">

938137d61e5ae6853a21bbabb76935a6d59e47d9
Flame1800 <r.shaficoff@gmail.com>
2024-12-01 03:01:31 +0500

    setup build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,8 @@ module.exports = {
                 tabWidth: 4,
             },
         ],
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off",
         "react/react-in-jsx-scope": "off",
         "react/require-default-props": [2, { functions: "defaultArguments" }],
         "import/prefer-default-export": "off",

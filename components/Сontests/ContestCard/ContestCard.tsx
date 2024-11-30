d686156fca68d78c484e90ffcada93f277038b48
Flame1800 <r.shaficoff@gmail.com>
2024-11-30 15:10:51 +0500

    update fonts and contest-card

--- components/Сontests/ContestCard/ContestCard.tsx
+++ components/Сontests/ContestCard/ContestCard.tsx
@@ -1,14 +1,17 @@
-import {Card} from "@/components/ui/card";
-import {Contest} from "@/components/Сontests/model";
+import { Card } from "@/components/ui/card";
+import { Contest } from "@/components/Сontests/model";
 
 interface Props {
-    contest: Contest
+    contest: Contest;
 }
 
-export const ContestCard = ({ contest }: Props) => {
-    const {title, subtitle} = contest
-    return <Card>
-        <p>{title}</p>
-        <p>{subtitle}</p>
-    </Card>
+export function ContestCard({ contest }: Props) {
+    const { title, subtitle } = contest;
+
+    return (
+        <Card className="p-6 bg-gray-100 border-none rounded-3xl">
+            <p className="text-2xl">{title}</p>
+            <p className="text-base text-gray-500">{subtitle}</p>
+        </Card>
+    );
 }

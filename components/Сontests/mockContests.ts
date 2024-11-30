8aa010e151eb8392606ce620639036334e735a14
Flame1800 <r.shaficoff@gmail.com>
2024-11-30 15:10:51 +0500

    update fonts and contest-card

--- components/Сontests/mockContests.ts
+++ components/Сontests/mockContests.ts
@@ -1,28 +1,28 @@
-import {Contest} from "@/components/Сontests/model";
+import { Contest } from "@/components/Сontests/model";
 
 export const mockContests: Contest[] = [
     {
         id: 1,
-        title: 'Test contest',
-        subtitle: 'Test contest',
+        title: "Гроссмейстеры завтрашнего дня ",
+        subtitle: "Развитие институтов гражданского общества города Когалыма",
         applicationsCount: 1,
         nominationsCount: 1,
-        organiser: 'organiser'
+        organiser: "organiser",
     },
     {
         id: 2,
-        title: 'Test contest',
-        subtitle: 'Test contest',
+        title: "Гроссмейстеры завтрашнего дня ",
+        subtitle: "Развитие институтов гражданского общества города Когалыма",
         applicationsCount: 1,
         nominationsCount: 1,
-        organiser: 'organiser'
+        organiser: "organiser",
     },
     {
         id: 3,
-        title: 'Test contest',
-        subtitle: 'Test contest',
+        title: "Гроссмейстеры завтрашнего дня ",
+        subtitle: "Развитие институтов гражданского общества города Когалыма",
         applicationsCount: 1,
         nominationsCount: 1,
-        organiser: 'organiser'
-    }
-]
+        organiser: "organiser",
+    },
+];
